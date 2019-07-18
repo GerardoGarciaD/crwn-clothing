92931fbf73f2b9a4872bae89c18d1ad41db38706
Gerardo Garcia <geraz26@hotmail.com>
2019-07-17 21:07:11 -0500

    Envolviendo el proyecto en BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Se importa el broswer router
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+
+  document.getElementById("root")
+);
