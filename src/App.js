f7269968cc327c4df46fa1519a9b90528a45882a
Gerardo Garcia <geraz26@hotmail.com>
2019-07-18 19:49:06 -0500

    Importando componentes y añadiendo rutas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import "./App.css";
 import HomePage from "./pages/homepage/HomePage";
+import ShopPage from "./pages/shop/Shop";
+import Header from "./components/header/Header";
+import SignInUp from "./pages/signInUp/SignInUp";
 
 // Se importa el componente Route
 import { Route, Switch } from "react-router-dom";
@@ -9,8 +12,11 @@ import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInUp} />
       </Switch>
     </div>
   );
