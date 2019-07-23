f4099febe3a693a31af56872e7bd332fb838bad7
Gerardo Garcia <geraz26@hotmail.com>
2019-07-22 21:11:58 -0500

    Login usando firebase

--- src/App.js
+++ src/App.js
@@ -6,20 +6,51 @@ import ShopPage from "./pages/shop/Shop";
 import Header from "./components/header/Header";
 import SignInUp from "./pages/signInUp/SignInUp";
 
+// Se importa el objeto para autenticar con google
+import { auth } from "./firebase/Firebase.utils";
+
 // Se importa el componente Route
 import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInUp} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  // "Variable"  con valor nulo  como "bandera" o valor por default
+  unsuscribeFromAuth = null;
+
+  // Se crea un lyfeCycledMethod para iniciar sesion
+  componentDidMount() {
+    // La variable this.unsuscribeFromAuth se iguala a la funcion para iniciar sesion
+    this.unsuscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  // Se crea un lyfeCycledMethod para mantener la sesion iniciada (cuando se desmonta el componente )
+  componentWillUnmount() {
+    this.unsuscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
