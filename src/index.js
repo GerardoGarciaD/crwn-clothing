2d26d9be711ebd998d852b2e8fc9c22a34ceb9cc
Gerardo Garcia <geraz26@hotmail.com>
2019-07-24 18:21:59 -0500

    Implementacion de Redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// Se importa el provider de react-redux
+import { Provider } from "react-redux";
+
 // Se importa el broswer router
 import { BrowserRouter } from "react-router-dom";
 
+// se obtiene toda la informacion para el proyecto y se manda como prop
+import store from "./redux/Store";
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 
   document.getElementById("root")
 );
