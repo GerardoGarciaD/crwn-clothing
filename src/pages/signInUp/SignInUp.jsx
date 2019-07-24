68d68b74bfef08bc1800fcb2ad8e8fe55a64140d
Gerardo Garcia <geraz26@hotmail.com>
2019-07-24 18:24:14 -0500

    Se integran los componentes para registro e inicio de sesion

--- src/pages/signInUp/SignInUp.jsx
+++ src/pages/signInUp/SignInUp.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import SignIn from "../../components/signIn/SignIn";
 
+import SignUp from "../../components/signUp/SignUp";
+import "./SignInUp.scss";
+
 export default function SignInUp() {
-  return <SignIn />;
+  return (
+    <div className="forms-SignIn-SignUp">
+      <SignIn />
+      <SignUp />
+    </div>
+  );
 }
