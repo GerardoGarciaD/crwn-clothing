f2ca3de5193bf9df8ec3fad091d59b4407098736
Gerardo Garcia <geraz26@hotmail.com>
2019-07-25 18:53:38 -0500

    Llamado a funcion para juntar items del mismo tipo en el carrito

--- src/redux/cart/CartReducer.js
+++ src/redux/cart/CartReducer.js
@@ -1,8 +1,15 @@
 // Se importan todas las acciones que existan en CartTypes
 import CartActionTypes from "./CartTypes";
 
+import { addItemToCart } from "./CartUtils";
+
+// Se importa la funcion que verifica si un producto ya estaba en el carrito o no
+
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+
+  // Array vacio que contendrá los items en el carrito
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,6 +18,15 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden
+      };
+
+    case CartActionTypes.ADD_ITEM:
+      return {
+        // Se regresa el estado "anterior" con toda la informacion que ya estaba antes
+        ...state,
+        // y se guarda el objeto cartItems con el nuevo valor que se ingresa en el reducer
+        // Aqui se manda a llamar la funcion que verifica si ya existe un item en especifico en el carrito
+        cartItems: addItemToCart(state.cartItems, action.payload)
       };
     default:
       return state;
