0a9473104f36a438f27a1a0a0ed206c16750e789
Gerardo Garcia <geraz26@hotmail.com>
2019-07-25 18:54:31 -0500

    Se agrega el nombre de la accion para el carrito

--- src/redux/cart/CartTypes.js
+++ src/redux/cart/CartTypes.js
@@ -1,7 +1,8 @@
 // Se crean y exportan los nombres para las acciones del carrito
 
 const CartActionTypes = {
-  TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN"
+  TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+  ADD_ITEM: "ADD_ITEM"
 };
 
 export default CartActionTypes;
