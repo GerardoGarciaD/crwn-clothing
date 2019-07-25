28bd46da63acb652ffb46b58fbacca856664fc2b
Gerardo Garcia <geraz26@hotmail.com>
2019-07-24 20:07:25 -0500

    Reducer para el carrito

---
+++ src/redux/cart/CartReducer.js
@@ -0,0 +1,20 @@
+// Se importan todas las cciones que existan en CartTypes
+import CartActionTypes from "./CartTypes";
+
+const INITIAL_STATE = {
+  hidden: true
+};
+
+const cartReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden
+      };
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
