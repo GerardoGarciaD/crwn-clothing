8cddeacd0ffd24bd923e2caec8f10ac5297986fd
Gerardo Garcia <geraz26@hotmail.com>
2019-07-25 18:53:09 -0500

    Funcion para juntar elementos del mismo tipo en el carrito

---
+++ src/redux/cart/CartUtils.js
@@ -0,0 +1,26 @@
+// En este archivo se van a crear funciones que pueden ser utilizadas al momento de agregar informacion a los reducers
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  // Se busca si el nuevo item que se quiere agregar, existe con anterioridad
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  //   Si, sí existe entonces se ejecuta este if
+  if (existingCartItem) {
+    //   en donde se recorren todos los items existentes del carrito
+    return cartItems.map(cartItem =>
+      // si el id del nuevo item coincide con el de un item que existe con anterioridad, entonces se le suma
+      // 1 a la cantidad de ese item
+      cartItem.id === cartItemToAdd.id
+        ? //   Si coincide se regresa todo todo el item, pero se suma la cantidad en 1
+          { ...cartItem, quantity: cartItem.quantity + 1 }
+        : // si el id no coincide se regresa el item sin modificar
+          cartItem
+    );
+  }
+
+  //   Si no existe el item en el carrito, entonces se regresa los items que ya estaban en el carrito
+  //   pero ademas se ingresa el nuevo item, con la nueva propiedad quantity
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
