63fc78bb760a9c6fc651421ac57925ccc26a8f9a
Gerardo Garcia <geraz26@hotmail.com>
2019-07-25 22:44:49 -0500

    Se implemento el selector para el numero de elementos en el carrito

--- src/components/cartIcon/CartIcon.jsx
+++ src/components/cartIcon/CartIcon.jsx
@@ -4,17 +4,20 @@ import { connect } from "react-redux";
 // Se importa la accion que esta relacionada con el Carrito
 import { toggleCartHidden } from "../../redux/cart/CartActions";
 
+// se importa el selector para obtener el numero total de elementos en el carrito
+import { selectCartItemsCount } from "../../redux/cart/CartSelectors";
+
 // Se importa el icono
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./CartIcon.scss";
 
 // Se reciben las props, que en este caso son las que se mandan de este mismp componente (High Order Component)
-function CartIcon({ toggleCartHidden }) {
+function CartIcon({ toggleCartHidden, itemCount }) {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 }
@@ -25,8 +28,15 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+// Se crea esta funcion para obtener el numero total de items en el carrito
+// se pasa como parametro todo el estado, ya que se utilizan selectors y se debe pasar todo el estado
+const mapStateToProps = state => ({
+  // Se regresa el numero de items usando el selector "selectCartItemsCount"
+  itemCount: selectCartItemsCount(state)
+});
+
 // Se crea el high order component en donde se envian las props
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
