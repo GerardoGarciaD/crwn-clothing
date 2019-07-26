8fb785786e8e8cc7af2b85cc5d2c4e923ce464e2
Gerardo Garcia <geraz26@hotmail.com>
2019-07-25 19:02:19 -0500

    Se implementa Redux

--- src/components/cartDropDown/CartDropDown.jsx
+++ src/components/cartDropDown/CartDropDown.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import CustomButton from "../customButon/CustomButton";
 
-import "./CartDropDown.scss";
+// Se obtiene la funcion para poder crear high order componets y hacer uso del state
+import { connect } from "react-redux";
 
-export default function CartDropDown() {
+import "./CartDropDown.scss";
+import CartItem from "../cartItem/CartItem";
+
+// Se hace destructuring del elemento que se quiere obtener del stado cuando se crea el  mapStateToProps
+function CartDropDown({ cartItems }) {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>Go To Checkout</CustomButton>
     </div>
   );
 }
+
+// Se obtiene la informacion del store
+// Se hace destructuring de la informacion,  en donde se obtiene el objeto cart y a su ves hace destructuring
+// para obtener el objeto cartItems
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  // Se regresa el objeto cartItems
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropDown);
