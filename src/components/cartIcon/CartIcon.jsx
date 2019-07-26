03108cc7fc857452aa5d6b1e9ad108fadcd6f59e
Gerardo Garcia <geraz26@hotmail.com>
2019-07-25 23:11:27 -0500

    Implementando la funcion createStructuredSelector

--- src/components/cartIcon/CartIcon.jsx
+++ src/components/cartIcon/CartIcon.jsx
@@ -4,6 +4,9 @@ import { connect } from "react-redux";
 // Se importa la accion que esta relacionada con el Carrito
 import { toggleCartHidden } from "../../redux/cart/CartActions";
 
+// Esta funcion permite mandar el state a cada selector de manera automatica
+import { createStructuredSelector } from "reselect";
+
 // se importa el selector para obtener el numero total de elementos en el carrito
 import { selectCartItemsCount } from "../../redux/cart/CartSelectors";
 
@@ -29,10 +32,10 @@ const mapDispatchToProps = dispatch => ({
 });
 
 // Se crea esta funcion para obtener el numero total de items en el carrito
-// se pasa como parametro todo el estado, ya que se utilizan selectors y se debe pasar todo el estado
-const mapStateToProps = state => ({
+//se utiliza la funcion createStructuredSelector que automaticamente manda el state a cada uno de los selectores
+const mapStateToProps = createStructuredSelector({
   // Se regresa el numero de items usando el selector "selectCartItemsCount"
-  itemCount: selectCartItemsCount(state)
+  itemCount: selectCartItemsCount
 });
 
 // Se crea el high order component en donde se envian las props
