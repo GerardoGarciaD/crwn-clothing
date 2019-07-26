dec0d4ce80161202daf8e1af8849441985f39edb
Gerardo Garcia <geraz26@hotmail.com>
2019-07-25 23:12:04 -0500

    Se utilizan los selectores para el usuario actual y para mostrar u ocultar el div del carrito tambien se utiliza la funcion createStructuredSelector

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,8 +15,14 @@ import CartIcon from "../cartIcon/CartIcon";
 // Se importa el CartDropDown
 import CartDropDown from "../cartDropDown/CartDropDown";
 
-import RootReducer from "../../redux/RootReducer";
-import userReducer from "../../redux/user/UserReducer";
+// Esta funcion permite mandar el state a cada selector de manera automatica
+import { createStructuredSelector } from "reselect";
+
+// se importa el selector para obtener el usuario actual
+import { selectCurrentUser } from "../../redux/user/UserSelectors";
+
+// se importa el selector que verifica si se muestra el div del carrito
+import { selectCartHidden } from "../../redux/cart/CartSelectors";
 
 // Se destructturing de las props de este mismo componente (ya que ahora es un High Order Component ) por que
 // se utilizo la funcion connect de react-redux
@@ -51,12 +57,12 @@ function Header({ currentUser, hidden }) {
   );
 }
 
-// Con esta funcion permite el acceso al estado general (RootRedcer)
-// se hace destructuring de los objetos que se desean obtener y los valores de esos objetos
-const mapsStateToProp = ({ user: { currentUser }, cart: { hidden } }) => ({
+// con la funcion createStructuredSelector se pasa automaticamente el state completo a cada selector que lo vaya a usar
+// (selectCurrentUser, selectCurrentUser)
+const mapsStateToProp = createStructuredSelector({
   // Como es una arrow function se regresan por default estos objetos
-  currentUser,
-  hidden
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 // Finalmete se exporta el componente como un highOrderCommponent
