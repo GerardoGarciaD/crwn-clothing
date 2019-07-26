5ee2b56f91bbedc3c86a9dcfe4268f635a2e5e82
Gerardo Garcia <geraz26@hotmail.com>
2019-07-25 22:45:36 -0500

    Selectors para obtener los items y el total del carrito

---
+++ src/redux/cart/CartSelectors.js
@@ -0,0 +1,27 @@
+// Estos selectores son importantes ya que son utilizados para mejorar el rendiemiento de la
+// pagina,ya que si se actualiza el estado y los componentes no utilizan la nueva informacioin, entonces no se vuelven a cargar
+import { createSelector } from "reselect";
+
+// Se selecciona una pequeña parte del estado, en este caso se selecciona el objeto cart
+const selectCart = state => state.cart;
+
+// Aqui se seleccionan los items del carrito
+export const selectCartItems = createSelector(
+  // se hace referencia a la variable selectCart que contiene todo el carrito
+  [selectCart],
+  //   se seleccionan todos los items
+  cart => cart.cartItems
+);
+
+// Se seleccionan o se cuentan todos los items del carrito
+export const selectCartItemsCount = createSelector(
+  // se hace referencia a la variable que contiene los items del carrito
+  [selectCartItems],
+  //   Se cuentan los elementos del carrito  utilizando reduce
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
