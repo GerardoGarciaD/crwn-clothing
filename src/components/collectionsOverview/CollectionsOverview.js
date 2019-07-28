85d091bc34ece13020ba5a2448b676b8132b4a7e
Gerardo Garcia <geraz26@hotmail.com>
2019-07-28 06:59:44 -0500

    Componente que muestra todas la categorias

---
+++ src/components/collectionsOverview/CollectionsOverview.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./CollectionsOverview.scss";
+
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import CollectionPreview from "../collectionPreview/CollectionPreview";
+import { selectCollections } from "../../redux/shop/ShopSelectors";
+
+function CollectionsOverview({ collections }) {
+  return (
+    <div className="collections-overview">
+      {/* Se recorre la "variable u objeto" collection */}
+      {/* Aqui se toman como parametros id, y se utiliza spread operator para
+        pasar las demas propiedades de cada objeto que es recorrido */}
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        // Se hace render al componente CollectionPreview y se mandan las props
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+}
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
