0dc840e5bb4d53b7889df29e672396cab2f86f29
Gerardo Garcia <geraz26@hotmail.com>
2019-07-28 06:59:16 -0500

    Separacion de componentes

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,33 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
-import CollectionPreview from "../../components/collectionPreview/CollectionPreview";
+import CollectionsOverview from "../../components/collectionsOverview/CollectionsOverview";
 
-import SHOP_DATA from "./ShopData";
-
-export default class Shop extends Component {
-  constructor(props) {
-    super(props);
-
-    // Se guarda la informacion de los productos
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    // Se crea una varible y se hace destructuring del estado
-    const { collections } = this.state;
-
-    return (
-      <div className="shop-page">
-        {/* Se recorre la "variable u objeto" collection */}
-        {/* Aqui se toman como parametros id, y se utiliza spread operator para
-        pasar las demas propiedades de cada objeto que es recorrido */}
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          // Se hace render al componente CollectionPreview y se mandan las props
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
+export default function Shop({ collections }) {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
 }
