410029ab530f28e4a64b2bd5e9c990807dd54c88
Gerardo Garcia <geraz26@hotmail.com>
2019-07-28 19:29:48 -0500

    Direccionamiento a grupos de productos

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
 import CollectionsOverview from "../../components/collectionsOverview/CollectionsOverview";
+import CollectionPage from "../collection/CollectionPage";
 
-export default function Shop({ collections }) {
+import { Route } from "react-router-dom";
+
+// Se obtiene la propiedad match, que se obtiene directamente desde el archivo App.js mediante route
+// en donde Route envia por defecto los parametros match, history y location
+export default function Shop({ match }) {
   return (
     <div className="shop-page">
-      <CollectionsOverview />
+      {/* match.path obtiene la el url exacto de la pagina que se esta desplegando ('/shop', '/cart', etc) */}
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+
+      {/* Para desplegar cada categoria de productos (hats, sneakers, etc.), primero se escribe la ubicacion actual con 
+      match.path y :categoryId se obtiene de la url ('shop/hats', 'shop/sneakers') y se manda como parametro para mostrar la pagina adecauda */}
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 }
