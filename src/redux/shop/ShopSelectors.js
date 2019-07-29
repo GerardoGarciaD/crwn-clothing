0ad12103e8ab8030d26d088ca78eca6d06936856
Gerardo Garcia <geraz26@hotmail.com>
2019-07-28 21:53:39 -0500

    Selector para obtener elementos de los grupos de producto

--- src/redux/shop/ShopSelectors.js
+++ src/redux/shop/ShopSelectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -15,13 +7,19 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
+// Selector en donde se convierte el un objeto a un array
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  // Se obtienen las "llaves" de cada grupo de elementos (hats, sneakers, etc) y apartir de esas llaves
+  // es obtienen cada uno de los productos de ese grupo
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
 // Esta funcion recibe dos parametros, en donde el primero es el id que se va a buscar
 // (hats, sneakers) y el segundo es el estado (state)
 export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
-    collections =>
-      collections.find(
-        collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-      )
+    collections => collections[collectionUrlParam]
   );
