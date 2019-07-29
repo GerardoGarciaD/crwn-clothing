97aa5a670c5da2abccb43ac075f25d3a48417911
Gerardo Garcia <geraz26@hotmail.com>
2019-07-28 21:56:35 -0500

    Cambio de props

--- src/pages/collection/CollectionPage.jsx
+++ src/pages/collection/CollectionPage.jsx
@@ -6,11 +6,16 @@ import { selectCollection } from "../../redux/shop/ShopSelectors";
 
 // Se obtienen las props que se mandan desde el hihg order component
 function CollectionPage({ collection }) {
-  console.log(collection);
+  const { title, items } = collection;
 
   return (
-    <div className="category">
-      <h2>Collection Page</h2>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 }
