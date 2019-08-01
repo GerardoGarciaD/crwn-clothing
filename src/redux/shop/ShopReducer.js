f9951a0c7fc093f8442c4246e0d7ca1ddc6460c0
Gerardo Garcia <geraz26@hotmail.com>
2019-07-31 23:19:53 -0500

    Añadiendo case a la accion que actualiza los productos de la tienda desde firebase

--- src/redux/shop/ShopReducer.js
+++ src/redux/shop/ShopReducer.js
@@ -1,5 +1,6 @@
 // Se importa la informacion inicial de la tienda
 import SHOP_DATA from "./ShopData";
+import ShopActionTypes from "./ShopTypes";
 
 // Se inicializa el estado con la informacion obtenida
 const INITIAL_STATE = {
@@ -8,6 +9,12 @@ const INITIAL_STATE = {
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    // Listening para la accion que actualiza el objeto collections del estado
+    case ShopActionTypes.UPDATE_COLLECTIONS:
+      return {
+        ...state,
+        collections: action.payload
+      };
     default:
       return state;
   }
