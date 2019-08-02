870fee32faf92f79d3ffe221732f9c618661262c
Gerardo Garcia <geraz26@hotmail.com>
2019-08-02 01:08:10 -0500

    Se añade el middleware thunk

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -6,10 +6,12 @@ import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
+import thunk from "redux-thunk";
+
 // se importa el reducer que contiene todos los reducers
 import rootReducer from "./RootReducer";
 
-const middlewares = [logger];
+const middlewares = [logger, thunk];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
