91f041ee3a4afaee81cd45cd453edca34cf896c6
Gerardo Garcia <geraz26@hotmail.com>
2019-08-10 20:57:06 -0500

    Implementando funcion para el listener de UserSagas

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -32,10 +32,12 @@ import { selectCurrentUser } from "../../redux/user/UserSelectors";
 
 // se importa el selector que verifica si se muestra el div del carrito
 import { selectCartHidden } from "../../redux/cart/CartSelectors";
+// Se importa la a accion que desencadena funciones del UserSagas
+import { signOutStart } from "../../redux/user/UserActions";
 
 // Se destructturing de las props de este mismo componente (ya que ahora es un High Order Component ) por que
 // se utilizo la funcion connect de react-redux
-function Header({ currentUser, hidden }) {
+function Header({ currentUser, hidden, signOutStart }) {
   return (
     <HeaderContainer className="header">
       <LogoContainer to="/">
@@ -49,7 +51,8 @@ function Header({ currentUser, hidden }) {
         </Link>
 
         {currentUser ? (
-          <OptionDiv className="option" onClick={() => auth.signOut()}>
+          // Se llama a la funcion que se obtiene de mapDispatchToProps
+          <OptionDiv className="option" onClick={signOutStart}>
             SIGN OUT
           </OptionDiv>
         ) : (
@@ -72,5 +75,13 @@ const mapsStateToProp = createStructuredSelector({
   hidden: selectCartHidden
 });
 
+// Se crea un objeto que consta de una funcion y que a su vez ejecuta una accion de UserActions
+const mapDispatchToProps = dispatch => ({
+  signOutStart: () => dispatch(signOutStart())
+});
+
 // Finalmete se exporta el componente como un highOrderCommponent
-export default connect(mapsStateToProp)(Header);
+export default connect(
+  mapsStateToProp,
+  mapDispatchToProps
+)(Header);
