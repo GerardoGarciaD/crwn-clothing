59eb03500ad4ad2c95fab9f6f0c6d5994e7b5b92
Gerardo Garcia <geraz26@hotmail.com>
2019-08-10 21:29:53 -0500

    Implementacion del middleware Sagas

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -6,14 +6,25 @@ import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
-import thunk from "redux-thunk";
-
+// Se importa funcion para crear el middleware para utilizar sagas
+import createSagaMiddleware from "redux-saga";
 // se importa el reducer que contiene todos los reducers
 import rootReducer from "./RootReducer";
+// Se importa el el file rootSaga que contiene todas las sagas
+import rootSaga from "./RootSaga";
 
-const middlewares = [logger, thunk];
+// Se crea el middleware para utilizar sagas
+const sagaMiddleWare = createSagaMiddleware();
+
+// import thunk from "redux-thunk";
+
+// Se añade el middleware al array de middlewares
+const middlewares = [logger, sagaMiddleWare];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// Se ejecuta el middleware con todas las sagas
+sagaMiddleWare.run(rootSaga);
 
 // Se crea una version de la informacion que podrá ser guardada en el navegador
 export const persistor = persistStore(store);
