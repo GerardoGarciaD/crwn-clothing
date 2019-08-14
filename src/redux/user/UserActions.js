3e87daaf15a7c09885c0343327c90bd959d08301
Gerardo Garcia <geraz26@hotmail.com>
2019-08-13 21:53:40 -0500

    Se añaden las funciones para Sagas

--- src/redux/user/UserActions.js
+++ src/redux/user/UserActions.js
@@ -1,7 +1,43 @@
 // Se importan las acciones del usuario
-import { UserActionTypes } from "./UserTypes";
+import UserActionTypes from "./UserTypes";
 
-export const setCurrentUser = user => ({
-  type: UserActionTypes.SET_CURRENT_USER,
+export const googleSignInStart = () => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_START
+});
+
+export const signInSuccess = user => ({
+  type: UserActionTypes.SIGN_IN_SUCCESS,
   payload: user
+});
+
+export const signInFailure = error => ({
+  type: UserActionTypes.SIGN_IN_FAILURE,
+  payload: error
+});
+
+export const emailSignInStart = emailAndPassword => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_START,
+  payload: emailAndPassword
+});
+
+export const checkUserSession = () => ({
+  type: UserActionTypes.CHECK_USER_SESSION
+});
+
+export const signOutStart = () => ({
+  type: UserActionTypes.SIGN_OUT_START
+});
+
+export const signOutSucces = () => ({
+  type: UserActionTypes.SIGN_OUT_SUCCES
+});
+
+export const signOutFailure = error => ({
+  type: UserActionTypes.SIGN_OUT_FAILURE,
+  payload: error
+});
+
+export const signUpStart = emailPasswordAndDisplayName => ({
+  type: UserActionTypes.SIGN_UP_START,
+  payload: emailPasswordAndDisplayName
 });
