486171ca511b320d185cba60291c5d78b4c14e74
Gerardo Garcia <geraz26@hotmail.com>
2019-08-13 21:40:22 -0500

    Se añade caso la accion de vaciar el carrito

--- src/redux/cart/CartReducer.js
+++ src/redux/cart/CartReducer.js
@@ -43,6 +43,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         )
       };
 
+    case CartActionTypes.CLEAR_CART:
+      return {
+        ...state,
+        cartItems: []
+      };
+
     default:
       return state;
   }
