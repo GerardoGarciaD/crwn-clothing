358534e525a1eaf6442738df2276cdd815fed17f
Gerardo Garcia <geraz26@hotmail.com>
2019-08-13 21:54:22 -0500

    Casos para las acciones con Sagas

--- src/redux/user/UserReducer.js
+++ src/redux/user/UserReducer.js
@@ -1,19 +1,34 @@
 // Se importan las acciones del usuario
-import { UserActionTypes } from "./UserTypes";
+import UserActionTypes from "./UserTypes";
 
 // Se crea el estado inicial
 const INITIAL_STATE = {
-  currentUser: null
+  currentUser: null,
+  error: null
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case UserActionTypes.SET_CURRENT_USER:
+    case UserActionTypes.SIGN_IN_SUCCESS:
       return {
         ...state,
-        currentUser: action.payload
+        currentUser: action.payload,
+        error: null
       };
 
+    case UserActionTypes.SIGN_OUT_SUCCES:
+      return {
+        ...state,
+        currentUser: null,
+        error: null
+      };
+
+    case UserActionTypes.SIGN_IN_FAILURE:
+    case UserActionTypes.SIGN_OUT_FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      };
     default:
       return state;
   }
